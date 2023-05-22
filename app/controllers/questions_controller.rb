c273e4193f8926f6f6bec85489bc6a479debd735
Katrina Gherzeddine <katgherzeddine@gmail.com>
2023-05-22 16:01:33 +0200

    app

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,6 +7,8 @@ class QuestionsController < ApplicationController
       @answer = "great"
     elsif params[:reponse].end_with?('?')
       @answer = "Silly question, get dressed and go to work!"
+    elsif params[:reponse].end_with?('e')
+      @answer = "C'est fou comme je m'en fou! ne me dérange plus! Sinon je te vire"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
